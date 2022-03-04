96b6165d55bc44456df98138cbf0443011370ac3
Sairam <sairamkaushik1@gmail.com>
2022-03-04 23:48:23 +0530

    added commits in variable.py

--- variable.py
+++ variable.py
@@ -1,5 +1,15 @@
+from pytest import ExitCode
+
+
 a = 10
 b = 20
 
 sum = (a+b)
 print(sum)
+
+
+#variables can be defined in :
+# 1) In Lower case
+# 2) In Upper case
+# 3) Cannot start a name with a digit
+# 4) _ etc
